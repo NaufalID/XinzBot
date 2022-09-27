d47a09378b1e383d606e1f18ff874a854f5d1237
NaufalID <88891630+NaufalID@users.noreply.github.com>
2022-09-27 13:19:16 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -10,14 +10,14 @@ let imgr = flaaa.getRandom()
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `𝙏𝙃𝘼𝙉𝙆𝙎 𝙏𝙊\n\n◐ Fokus ID\n◐FangzXD\n◐The.sad.boy01\n◐Kanna\n◐Nurutomo\n◐Bochilgaming\n◐Amirul\n◐Ilham\n◐Rasel\n◐Pemakai bot ini\n◐I love you\n\n© https://bit.ly/3QJOy9F
- `,dmenuf + '\n\n' + cmenua, `${imgr + 'Thanks To'}`, [['THNKS FANGZ','Ok']], m, {
+ await conn.sendButton(m.chat, `𝙏𝙃𝘼𝙉𝙆𝙎 𝙏𝙊\n\n◐ Fokus ID\n◐Dzaky\n◐Hanifah\n◐Kanna\n◐Nurutomo\n◐Bochilgaming\n◐Amirul\n◐Ilham\n◐Rasel\n◐Pemakai bot ini\n◐I love you\n\n© Naufal Muflih
+ `,dmenuf + '\n\n' + cmenua, `${imgr + 'Thanks To'}`, [['THNKS Naufal Muflih','Ok']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: 'Contributor FangzXD',          previewType: 0,
+                        body: 'Contributor Naufal Muflih',          previewType: 0,
                         jpegThumbnail: await (await fetch(thumbdoc)).buffer(),
                         sourceUrl: swb
                       }}
